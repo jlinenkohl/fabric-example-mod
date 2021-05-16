efc9b433634f67e905cd9c540537c5ea63911dc0
jlinenkohl <jlinenkohl@gmail.com>
2021-05-16 00:11:45 -0400

    Rollback from upstream change in master (62a3d3c).

---
+++ remappedSrc/net/fabricmc/example/ExampleMod.java
@@ -0,0 +1,25 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.*;
+import net.minecraft.util.*;
+import net.minecraft.util.registry.*;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+
+		// Register the new item
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
+
+	}
+	public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+//	public static final FabricItem FABRIC_ITEM = new FabricItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16));
+
+}
